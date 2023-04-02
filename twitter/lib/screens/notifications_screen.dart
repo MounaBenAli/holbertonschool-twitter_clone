74712b925a424978de642a619a59073373b35d47
MounaBenAli <benalimouna1986@gmail.com>
2023-04-02 18:31:57 +0200

    0x03. Flutter - Twitter Clone Static Part 2

---
+++ twitter/lib/screens/notifications_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class NotificationsScreen extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _NotificationsScreenState createState() => _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // Replace this with code
+  }
+}
